41ab5dad5c64a18f8c24b7f106c267095d8d8a4b
Sunisa Kanchanasuwan <sunisa.kanchanasuwan@atea.se>
2020-09-11 17:03:16 +0200

    Add function turn

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -5,34 +5,74 @@ import './bootstrap.min.css';
 
 function Hero() {
   return (<div className="row">
-    <div className ="jumbotron col-10 offset-1">
+    <div className="jumbotron col-10 offset-1">
       <h1>Author Quiz</h1>
       <p>Select the book written by the author shown</p>
     </div>
-</div>)
+  </div>);
 }
 
-function Turn() {
-  return (<div/>);
+function Book({title, onClick}) {
+  return (<div className="answer" onClick={() => {onClick(title);}}>
+    <h4>{title}</h4>
+  </div>
+  );
 }
+
+function Turn({author, books, highlight, onAnswerSelected}) {
+  function highlightToBgColor(highlight) {
+    const mapping = {
+      'none': '',
+      'correct': 'green',
+      'wrong': 'red'
+    };
+    return mapping[highlight];
+  }
+
+  return (<div className="row turn" style={{backgroundColor: highlightToBgColor(highlight)}}>
+    <div className="col-4 offset-1">
+      <img src={author.imageUrl} className="authorimage" alt="Author"/>
+    </div>
+    <div className="col-6">
+      {books.map((title) => <Book title={title} key={title} onClick={onAnswerSelected} />)}
+    </div>
+  </div>);
+}
+Turn.propTypes = {
+  author: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+    imageSource: PropTypes.string.isRequired,
+    books: PropTypes.arrayOf(PropTypes.string).isRequired
+  }),
+  books: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onAnswerSelected: PropTypes.func.isRequired,
+  highlight: PropTypes.string.isRequired
+};
 
 function Continue() {
   return (<div/>);
 }
 
 function Footer() {
-  return (<div/>);
+  return (<div id="footer" className="row">
+    <div className="col-12">
+        <p className="text-muted credit">
+          All images are from <a href="http://commons.wikimedia.org/wiki/Main_Page">Wikemedia Commons</a> and are in the public domain
+        </p>
+    </div>
+  </div>);
 }
-class AuthorQuiz extends Component {
-  render() {
+
+function AuthorQuiz({turnData, highlight, onAnswerSelected}) {
   return (
     <div className="container-fluid">
-      <Hero/> 
-      <Turn/> 
-      <Continue /> 
+      <Hero />
+      <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected} />
+      <Continue />
       <Footer />
     </div>
-  )
+  );
 }
-}
+
 export default AuthorQuiz;
